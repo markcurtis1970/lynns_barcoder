df3957e688b31b487a49d661666207e77df8c7e7
Mark Curtis <mark.curtis@datastax.com>
2015-10-24 13:55:27 +0100

    Update barcode_ui.py

--- barcode_ui.py
+++ barcode_ui.py
@@ -9,33 +9,40 @@ attributes = sys.argv
 entries = []
 labels = []
 
-# Functions
+# *** Functions ***
 
+# check there is a barcode before reading
+def checkCode():
+    if (entries[0].get() == ""):
+        print "no entry"
+    else:
+        print entries[0].get()
+
+# read from the data source
 def read ():
     print "read command"
+    checkCode();
 
+# write to the data source
 def write ():
     print "write command"
 
-# For testing
-#attributes = {'Field1', 'Field2', 'Field3', 'Field4', 'Field5', 'Field6'}
+# *** create list and text boxes ***
 
-# Generate the list and text boxes
-#
 # enumerate the list of attributes (number, value)
 for i,a in enumerate(attributes):
     # exclude the first item which is the file name
     if i > 0 :
         myLabel = Label(root, text = a)
-        myLabel.grid(column=0, row = i)
+        myLabel.grid(column = 0, row = i)
         labels.append(myLabel)
         myEntry = Entry(root)
-        myEntry.grid(column=1, row=i)
+        myEntry.grid(column = 1, row = i)
         entries.append(myEntry)
         # use i-1 as we had to negate the first entry
         entries[i-1].insert(INSERT,"text to insert")
 
-# Generate the buttons
+# *** create buttons ***
 
 myRead = Button ( root, command = read, text = "Read")
 myRead.grid(column = 0, row = i +1)
