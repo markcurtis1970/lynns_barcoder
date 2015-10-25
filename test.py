bcd35fa9d40b5b59efd33f3b76edda8ca6bd6583
markcurtis1970 <mark.curtis@datastax.com>
2015-10-25 20:23:35 +0000

    general updates

--- test.py
+++ test.py
@@ -3,9 +3,9 @@
 import sys
 import os
 from db_comms import connect, disconnect, exec_read, exec_write
+from export import export
 import ConfigParser
 
-'''
 connect()
 exec_write("insert into items values ('val1', 'val1')")
 exec_write("insert into items values ('val2', 'val2')")
@@ -15,14 +15,13 @@ exec_write("insert into items values ('val5', 'val5')")
 exec_write("insert into items values ('val6', 'val6')")
 exec_write("insert into items values ('val7', 'val7')")
 results = exec_read("select * from items")
-for result in results:
-    print result
 disconnect()
+
+print results
+for line in results:
+    print line[0], line[1]
+
 '''
-
-config = ConfigParser.RawConfigParser()
-config.read('bc.properties')
-myconf = config.items('ui properties')
-for myitem in myconf:
-    print myitem[0],"=", myitem[1]
-
+print "export test..."
+export(results)
+'''
