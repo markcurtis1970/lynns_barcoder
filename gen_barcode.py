6421a94201a725cc2419d0eb65fe9086016f5bf1
markcurtis1970 <mark.curtis@datastax.com>
2015-11-03 20:57:42 +0000

    Added module for normal barcode types

---
+++ gen_barcode.py
@@ -0,0 +1,12 @@
+# For the QR generation part of the barcode reader
+# depends on qrencode to be installed
+
+import sys
+import os
+
+def make_code(code):
+    mycode = code
+    myfile = code + ".ps"
+    # using the following command format
+    # barcode -e 128 -b "Hello World!" -o barcode.ps
+    os.system("barcode -e 128"  + " -b " + mycode + " -o " + myfile)
